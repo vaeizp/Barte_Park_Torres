4ca655c015c2d9e34f7590be487ac2414067830a
p <jppark@carc.pshs.edu.ph>
2026-04-22 13:39:55 +0800

    fix

--- Planner Code Minimal.py
+++ Planner Code Minimal.py
@@ -18,31 +18,41 @@ except:
     tasks = []
     print("You currently have no tasks to finish.\n")
 
-
 # -------------------------
 # Main while loop
 # -------------------------
-print(" .----------------.  .----------------.  .----------------.  .-----------------. .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.")
+print(
+    " .----------------.  .----------------.  .----------------.  .-----------------. .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.")
 time.sleep(0.25)
-print(" | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |")
+print(
+    " | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |")
 time.sleep(0.25)
-print(" | |   ____       | || |   ___        | || |       _      | || |    ___  ___  | || |              | || |      __      | || |              | || |  ______      | || |     _ ,      | || |  ___    __   | || |      __      | |")
+print(
+    " | |   ____       | || |   ___        | || |       _      | || |    ___  ___  | || |              | || |      __      | || |              | || |  ______      | || |     _ ,      | || |  ___    __   | || |      __      | |")
 time.sleep(0.25)
-print(" | |  |_   _ ,    | || |  |   |       | || |     / , \\    | || |  |   ,|_   | | || |              | || |   .'    `.   | || |              | || | |   _   ,    | || |    /   ,     | || | |  ,  /  _|  | || |     /  \\     | |")
+print(
+    " | |  |_   _ ,    | || |  |   |       | || |     / , \\    | || |  |   ,|_   | | || |              | || |   .'    `.   | || |              | || | |   _   ,    | || |    /   ,     | || | |  ,  /  _|  | || |     /  \\     | |")
 time.sleep(0.25)
-print(" | |  | |__) |    | || |   | |        | || |    / /,\\ \\   | || |  |   , | |   | || |    ______    | || |  /  .--.  ,  | || |    ______    | || |  | |__) |    | || |   / /,'  ,   | || | | | , /|  |  | || |    / /\\ ,    | |")
+print(
+    " | |  | |__) |    | || |   | |        | || |    / /,\\ \\   | || |  |   , | |   | || |    ______    | || |  /  .--.  ,  | || |    ______    | || |  | |__) |    | || |   / /,'  ,   | || | | | , /|  |  | || |    / /\\ ,    | |")
 time.sleep(0.25)
-print(" | |  |  __/      | || |  | |   _     | || |   /  ___, \\  | || |  | |, ,| |   | || |   |______|   | || |  | |    | |  | || |   |______|   | || |  |  _ /      | || |  /  ___   ,  | || | | |  , | |   | || |   / /__\\ \\   | |")
+print(
+    " | |  |  __/      | || |  | |   _     | || |   /  ___, \\  | || |  | |, ,| |   | || |   |______|   | || |  | |    | |  | || |   |______|   | || |  |  _ /      | || |  /  ___   ,  | || | | |  , | |   | || |   / /__\\ \\   | |")
 time.sleep(0.25)
-print(" | |  | |         | || |  | |__/ |    | || | _/ /_   ,  \\ | || | | |,   |_ _  | || |              | || |  ,  `--'  /  | || |              | || |  | |  , |_   | || | /  /   /   , | || | | |, _|  |_  | || | _/ /    , ,_ | |")
+print(
+    " | |  | |         | || |  | |__/ |    | || | _/ /_   ,  \\ | || | | |,   |_ _  | || |              | || |  ,  `--'  /  | || |              | || |  | |  , |_   | || | /  /   /   , | || | | |, _|  |_  | || | _/ /    , ,_ | |")
 time.sleep(0.25)
-print(" | |  |_____|     | || |  |________|  | || ||____|  |____|| || | |___|,____|  | || |              | || |   `.____.'   | || |              | || | |___|  |___| | || ||____|  |____|| || ||_____||_____|| || ||____|  |____|| |")
+print(
+    " | |  |_____|     | || |  |________|  | || ||____|  |____|| || | |___|,____|  | || |              | || |   `.____.'   | || |              | || | |___|  |___| | || ||____|  |____|| || ||_____||_____|| || ||____|  |____|| |")
 time.sleep(0.25)
-print(" | |              | || |              | || |              | || |              | || |              | || |              | || |              | || |              | || |              | || |              | || |              | |")
+print(
+    " | |              | || |              | || |              | || |              | || |              | || |              | || |              | || |              | || |              | || |              | || |              | |")
 time.sleep(0.25)
-print(" | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |")
+print(
+    " | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |")
 time.sleep(0.25)
-print(" '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'")
+print(
+    " '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'")
 a = True
 print("Welcome...")
 time.sleep(0.5)
@@ -52,21 +62,34 @@ print("\n")
 
 time.sleep(1.5)
 print("=" * 100)  # Cute little loading bar I wanted to add
-print("Loading tracker assets")
+print("Loading tracker assets...")
 print("\nPlease wait")
 count = 0
 bar_length = 10
+
+print("Have you ever felt the guilt of not accomplishing your requirements you could've sworn you will do but just forgot? ")
+time.sleep(2)
+print("Well worry no more because our program will help you finish all of your tasks without forgetting anything.\nPlan-O-Rama is a program that allows users to input, remove or view tasks with actual date, urgency, time, subject, and other requirement details. ")
+time.sleep(5)
+print("Choice 1 will ask you to insert the subject, task name, requirement details, urgency, deadline and an option\n to also add the specific time of submission or not. If you have entered a wrong format it will display a text that tells\n you to input it in the right format with an example. ")
+time.sleep(4)
+print("Choice 2 allows you to have an overview of all the requirements you added and if you have no pending requirements\n it displays a texts that encourages you to add some so that you do not have to cram. ")
+time.sleep(3)
+print("Choice 3 wil display all your tasks and ask you which would you want to mark complete accordingly it will remove\n the said task. ")
+time.sleep(2)
+print("Choice 4 basically terminates the whole program. ")
+
+print("\nWelcome, humble student! What would you like to do?")
+print("\n")
+time.sleep(1)
 while count < bar_length:
-    w = random.randint(1,3)
+    w = random.randint(1, 2)
     print("█", end="█")
-    time.sleep((w - 0.5))
+    time.sleep((w - 0.7))
     count += 1
 while a == True:
 
-    print("\nWelcome, humble student! What would you like to do?")
-    print("\n")
-    time.sleep(1)
-    print("1 - Add Task")
+    print("\n1 - Add Task")
     time.sleep(0.5)
     print("2 - View Already Added Tasks")
     time.sleep(0.5)
@@ -117,7 +140,7 @@ while a == True:
         time.sleep(0.5)
 
         # -------------------------
-        # Requirement Details (this is completely optional, but i cleaned it up to match the format)
+        # Requirement Details (this is completely optional, but I cleaned it up to match the format)
         # -------------------------
         requirement = input("Requirement Details: ")
         time.sleep(0.5)
@@ -128,7 +151,8 @@ while a == True:
         while True:
             deadline = input("Deadline (MM/DD/YY): ")
             try:
-                datetime.strptime(deadline, "%m/%d/%y") # .strptime helps convert a string representation of a specific date into a Python object (datetime)
+                datetime.strptime(deadline,
+                                  "%m/%d/%y")  # .strptime helps convert a string representation of a specific date into a Python object (datetime)
                 break
             except ValueError:
                 print("Wait! Use the format MM/DD/YY (e.g., 04/15/26). Let's do that again!")
@@ -140,15 +164,15 @@ while a == True:
         # Exact Time (format is strictly HH:MM AM/PM)
         # -------------------------
         while True:
-            exact_time_deadline = input("Exact Time (HH:MM AM/PM): ")
-            if exact_time_deadline == "":
-                exact_time_deadline = "N/A"
+            exact_time = input("Exact Time (HH:MM AM/PM) or leave blank: ")
+            if exact_time == " ":
+                exact_time = "N/A"
                 break
             try:
-                datetime.strptime(exact_time_deadline, "%I:%M %p")
+                datetime.strptime(exact_time, "%I:%M %p")
                 break
             except ValueError:
-                print("Hold up! Use HH:MM AM/PM. Please do not use the 24-hour time format. Let's try that again!")
+                print("Use HH:MM AM/PM (e.g., 11:59 PM). No 24-hour time! Let's try again!")
                 time.sleep(1)
 
         time.sleep(0.5)
@@ -173,13 +197,13 @@ while a == True:
             "task_name": task_name,
             "requirement": requirement,
             "deadline": deadline,
-            "time": exact_time_deadline,
+            "time": exact_time,
             "urgency": urgency,
             "status": status
         }
 
         tasks.append(task)
-        
+
         with open(filename, "w") as file:
             json.dump({"tasks": tasks}, file, indent=4)
 
@@ -192,7 +216,16 @@ while a == True:
         if len(tasks) == 0:
             print("You've accomplished all your tasks. So proud of you!")
         else:
-            print(tasks)
+            for i, task in enumerate(tasks, 1):
+                print(f"\nTask #{i}")
+                print(f"Subject   : {task['subject']}")
+                print(f"Task Name : {task['task_name']}")
+                print(f"Details   : {task['requirement']}")
+                print(f"Deadline  : {task['deadline']} at {task['time']}")
+                print(f"Urgency   : {task['urgency']}")
+                print(f"Status    : {task['status']}")
+                print("-" * 40)
+                time.sleep(2)
 
     elif choice == "3":
         if len(tasks) == 0:
@@ -212,6 +245,8 @@ while a == True:
                 print("That task number does not exist. Please enter another number.")
                 time.sleep(1)
                 print("Returning to main menu...")
+            if tasks[task_accom - 1]["status"] == "Completed":
+                tasks.remove(tasks[task_accom - 1])
     elif choice == "4":
         print(f"Terminating the Program", end="")
         time.sleep(0.5)
@@ -219,6 +254,7 @@ while a == True:
         time.sleep(0.5)
         print(f".", end="")
         time.sleep(0.5)
-        print(f".", end="")
+        print(f".")
+        print("Thank you for using Plan-O-Rama! Goodbye and we hope to see you again!:)")
         time.sleep(1)
         a = False
